eb2170fb0a87333cba52ee60711b8d9016aa033a
Johann Williams <johann.williams01@gmail.com>
2023-04-14 09:34:50 -0500

    updated external js to use a confirm message as well as a prompt for testing their use and outputs with a console log.

--- js/external.js
+++ js/external.js
@@ -3,4 +3,14 @@
 console.log("Hello from external JavaScript");
 
 
-alert("Message goes here.");
+//alert("Message goes here.");
+
+
+// // the following line will show the OK/CANCEL confirm dialog
+// var confirmed = confirm('Are you sure you want to do XYZ?');
+// console.log(confirmed); // will be either true or false
+
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
+

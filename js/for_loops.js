b4d03dde6ae422876e0edec89d4e2ce32cbc9111
Johann Williams <johann.williams01@gmail.com>
2023-04-19 15:10:22 -0500

    completed the showMultiplicationTable function. takes a number and logs a times table from 1 to 10 of that number.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,5 +1,12 @@
 (function (){
     "use strict";
 
+    function showMultiplicationTable(num){
+        for(let i = 1; i <= 10; i++){
+            console.log(num + " x " + i + " = " + (num * i));
+        }
+    }
+
+    showMultiplicationTable(parseFloat(prompt("Enter a number for a Times Table from 1 to 10 of that number!")));
 
 })();

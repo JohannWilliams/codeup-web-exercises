c18b2fd77a7b4d4e3acd3175c59a3b50af516a34
Johann Williams <johann.williams01@gmail.com>
2023-04-19 15:14:44 -0500

    completed next section, display even/odd for 10 randome numbers from 20-200

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,6 +1,10 @@
 (function (){
     "use strict";
 
+    /**
+     * start of problem. times table. 1 - 10
+     * @param num
+     */
     function showMultiplicationTable(num){
         for(let i = 1; i <= 10; i++){
             console.log(num + " x " + i + " = " + (num * i));
@@ -8,5 +12,37 @@
     }
 
     showMultiplicationTable(parseFloat(prompt("Enter a number for a Times Table from 1 to 10 of that number!")));
+    /**
+     * end of problem
+     */
+
+
+
+    /**
+     * start of next problem display even odd for 10 random numbers from 20-200
+     */
+
+    function isEven(input){
+        let even = false;
+        if(!isNaN(input) && input !== Infinity && input !== true && input !== false && input != null){
+            if(input % 2 === 0){
+                even = true;
+            }
+        }
+        return even;
+    }
+
+    for(let i = 0; i < 10; i++){
+        let random = Math.floor((Math.random() * 180) + 20);
+        if(isEven(random)){
+            console.log(random + " is even!");
+        } else {
+            console.log(random + " is odd!");
+        }
+    }
+
+    /**
+     * end of problem
+     */
 
 })();

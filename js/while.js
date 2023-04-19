f134462c6b08ee8742d2629492d4a7702bcb0ebd
Johann Williams <johann.williams01@gmail.com>
2023-04-19 15:33:10 -0500

    completed first part of while excercise. log the powers of 2 from 2-65536.

--- js/while.js
+++ js/while.js
@@ -1,5 +1,17 @@
 (function (){
     "use strict";
 
+    /**
+     * start of while problem.
+     * print the powers of 2 starting with 2 until 65536
+     */
+    let i = 1;
+
+    while(2**i < 70000){
+        console.log(2**i);
+        i++;
+    }
+
+
 
 })();

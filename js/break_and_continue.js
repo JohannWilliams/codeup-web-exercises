add92b454fd8af25c43956630f3cb24f69d3ac7f
Johann Williams <johann.williams01@gmail.com>
2023-04-19 15:29:42 -0500

    completed break and continue excercise. prompt user for odd # between 1 and 50. prints all odd #'s besides the number given. User is promped if incorrect input is given.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,33 @@
 (function (){
     "use strict";
 
+    let num = parseInt(prompt("Enter an odd number from 1 to 50"));
+
+    /**
+     * NOTE:
+     * the while loop can have a condition to check for the correct input type
+     * and if the correct input type is not given then you just prompt the user
+     * again. The prompt will display until the condition is met for a correct
+     * input.
+     */
+    while(true){
+        if(isNaN(num) || num % 2 === 0 || num < 1 || num > 50 || num === undefined){
+            num = parseInt(prompt("Incorrect input please try again. Odd Number between 1 and 50"));
+        } else {
+            break;
+        }
+    }
+
+    console.log("Number to skip is: " + num);
+
+    for(let i = 1; i <= 50; i++){
+        if(i % 2 !== 0) {
+            if(i === num){
+                console.log("Yikes! Skipping number: " + i);
+                continue;
+            }
+            console.log("Here is an odd number: " + i);
+        }
+    }
 
 })();

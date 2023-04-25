9792e541cb3e89d1070a71213f9fd347029653fd
Johann Williams <johann.williams01@gmail.com>
2023-04-25 13:53:07 -0500

    completed all parts of math exercise. getArea, logInfo, round if doRound === true, and update radius to 5.

--- js/circle.js
+++ js/circle.js
@@ -2,14 +2,15 @@
     "use strict";
 
     // create a circle object
-    var circle = {
+    let circle = {
         radius: 3,
 
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
+            let area = Math.PI * this.radius**2;
 
-            return; // TODO: return the proper value
+            return area; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
@@ -17,8 +18,13 @@
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
+            let area = this.getArea();
+            if(doRounding)
+            {
+                area = Math.round(area);
+            }
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
     };
 
@@ -30,6 +36,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");

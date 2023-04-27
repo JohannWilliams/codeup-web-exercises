1094013825eeb8a6fc61dd3bb87763bde66eee06
Johann Williams <johann.williams01@gmail.com>
2023-04-27 09:22:18 -0500

    created warm-up js and html file, completed warm-up 1

---
+++ js/warm-up.js
@@ -0,0 +1,16 @@
+(function (){
+    "use strict";
+
+    /**
+     * Warmup 1
+     * make a function that returns the number 7
+     * @returns {number}
+     */
+
+    function returnSeven(){
+        return 7;
+    }
+
+    console.log(`Calling returnSeven function to get: ${returnSeven()}`);
+
+})();

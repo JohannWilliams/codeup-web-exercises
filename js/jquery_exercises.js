4371dd67a4131df6cb3fb006513eda837ad3c5e0
Johann Williams <johann.williams01@gmail.com>
2023-05-23 13:42:04 -0500

    completed jquery mouse events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,6 +1,24 @@
 "use strict"
 
 $(function(){
-    $("li").css("font-size", "20px");
-    $("h1, p, li").css("background", "yellow");
+    $("h1").click(
+        function (){
+            $(this).css("background", "gray");
+        }
+    );
+
+    $("p").dblclick(
+        function (){
+            $(this).css("font-size", "18px");
+        }
+    );
+
+    $("li").hover(
+        function() {
+            $(this).css("color", "red");
+        },
+        function() {
+            $(this).css("color", "black");
+        }
+    );
 });

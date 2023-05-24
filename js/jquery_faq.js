1d7377af80a63bbe9d232b1c5df9ec8414612f40
Johann Williams <johann.williams01@gmail.com>
2023-05-24 12:20:58 -0500

    updated and completed essential FAQ exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,6 +1,19 @@
 "use strict";
 
 $(function (){
+    /**
+     * add event of click to each title to toggle highlighting
+     * the specific element(dt) that was clicked.
+     */
+    $("dt").on("click", function (){
+        $(this).toggleClass("highlight");
+    });
 
-
+    /**
+     * add event of click to link at bottom of list. when
+     * clicked, all dd's are toggled between visible/invisible
+     */
+    $(".toggle-visibility").on("click", function (){
+        $("dd").toggleClass("invisible");
+    });
 });

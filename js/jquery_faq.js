5b0819f1fcc1d2831cfa15cc840a7e29614b5829
Johann Williams <johann.williams01@gmail.com>
2023-05-24 13:41:33 -0500

    updated and FAQ exercise to add a link to page from JQuery

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,6 +1,11 @@
 "use strict";
 
 $(function (){
+    let linkToAdd = `<a class="toggle-visibility" href="#">Show/Hide Answers</a>`
+
+    //add link to div section below list
+    $(".add-link").append(linkToAdd);
+
     /**
      * add event of click to each title to toggle highlighting
      * the specific element(dt) that was clicked.

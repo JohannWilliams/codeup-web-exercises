2b697f00447fded98e0af816b39ff68190d0530f
Johann Williams <johann.williams01@gmail.com>
2023-05-30 11:50:42 -0500

    added my favorite restaurant and a marker with a popup when clicked

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -2,11 +2,24 @@
 
 $(document).ready(function(){
     mapboxgl.accessToken = MAPBOX_KEY_EX_1;
-    var map = new mapboxgl.Map({
+    let map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
         zoom: 10, // starting zoom
         center: [-98.4916, 29.4252] // [lng, lat]
+    });
+
+    geocode("Vorstadt 11, 67292 Kirchheimbolanden, Germany", MAPBOX_KEY_EX_1).then(function(result) {
+        console.log(result);
+        map.setCenter(result);
+        map.setZoom(15);
+        let marker = new mapboxgl.Marker()
+            .setLngLat(result)
+            .addTo(map);
+        let restaurantPopup = new mapboxgl.Popup()
+            .setHTML("<p>Sapientia Restaurant Inh. Nursel Inci</p>")
+
+        marker.setPopup(restaurantPopup)
     });
 
 });

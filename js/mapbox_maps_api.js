dc553fd3c7ccaa34ba7cc001a7d8f2ddca2e42c1
Johann Williams <johann.williams01@gmail.com>
2023-05-30 13:39:54 -0500

    added a arr of restaurant objects. display map with 3 markers with popups

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -4,22 +4,52 @@ $(document).ready(function(){
     mapboxgl.accessToken = MAPBOX_KEY_EX_1;
     let map = new mapboxgl.Map({
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL
         zoom: 10, // starting zoom
-        center: [-98.4916, 29.4252] // [lng, lat]
+        pitch: 75,
+        bearing: 35,
+        center: [7.725, 49.47] // [lng, lat]
     });
 
-    geocode("Vorstadt 11, 67292 Kirchheimbolanden, Germany", MAPBOX_KEY_EX_1).then(function(result) {
-        console.log(result);
-        map.setCenter(result);
-        map.setZoom(15);
-        let marker = new mapboxgl.Marker()
-            .setLngLat(result)
-            .addTo(map);
-        let restaurantPopup = new mapboxgl.Popup()
-            .setHTML("<p>Sapientia Restaurant Inh. Nursel Inci</p>")
+    let restaurantsArr = [
+        {
+            name: "restaurant SAPIENTIA",
+            address: "Vorstadt 11, 67292 Kirchheimbolanden, Germany",
+            times: " Dienstag - Sonntag<br>" +
+                "* 11:30 - 14:00 Uhr<br>" +
+                "* 17:30 - 22:00 Uhr<br>" +
+                " Montag Ruhetag<br>" +
+                "* (an Feiertagen geöffnet)"
+        },{
+            name: "Döner Time",
+            address: "Kaiserstraße 1, 67661 Kaiserslautern, Germany",
+            times: " Monday - Saturday<br>" +
+                "* 11:00 - 21:00 Uhr<br>" +
+                " Sunday<br>" +
+                "* 12:00 - 21:00 Uhr"
+        },{
+            name: "Pizza Hut Kaiserslautern, Kaiserstraße",
+            address: "Kaiserstraße 34, 67661 Kaiserslautern, Germany",
+            times: " Monday - Thursday<br>" +
+                "* 11:00 - 22:00 Uhr<br>" +
+                " Friday - Saturday<br>" +
+                "* 11:00 - 00:00 Uhr<br>" +
+                " Sunday<br>" +
+                "* 12:00 - 22:00 Uhr"
+        }
+    ];
 
-        marker.setPopup(restaurantPopup)
+    restaurantsArr.forEach(function(restaurant){
+        geocode(restaurant.address, MAPBOX_KEY_EX_1).then(function(result) {
+            console.log(result);
+            map.setZoom(12);
+            let marker = new mapboxgl.Marker()
+                .setLngLat(result)
+                .addTo(map);
+            let restaurantPopup = new mapboxgl.Popup()
+                .setHTML("<p>" + restaurant.name + "<hr>" + restaurant.times + "</p>");
+
+            marker.setPopup(restaurantPopup);
+        });
     });
-
 });

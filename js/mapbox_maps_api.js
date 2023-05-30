6d275733f552b7932629976633cb4efcf7b9c105
Johann Williams <johann.williams01@gmail.com>
2023-05-30 16:05:42 -0500

    added ability to change map style, search by address, zoom in/out and navigate to specific restaurant or back to home orientation and position

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,14 +1,27 @@
 "use strict";
 
 $(document).ready(function(){
+
+    let darkMapStyle = 'mapbox://styles/mapbox/navigation-night-v1';
+    let lightMapStyle = 'mapbox://styles/mapbox/navigation-day-v1';
+    let homeLatLong = [7.725, 49.47];
+    let homeZoom = 12
+    let homePitch = 75;
+    let homeBearing = 35;
+    let mapCenterLocation = homeLatLong;
+
+    let currentZoomIndex = 4;
+    let zoomArr = [0, 5, 10, 12, 15, 17, 20];
+
+
     mapboxgl.accessToken = MAPBOX_KEY_EX_1;
     let map = new mapboxgl.Map({
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL
-        zoom: 10, // starting zoom
-        pitch: 75,
-        bearing: 35,
-        center: [7.725, 49.47] // [lng, lat]
+        style: darkMapStyle, // style URL
+        zoom: homeZoom, // starting zoom
+        pitch: homePitch,
+        bearing: homeBearing,
+        center: homeLatLong // [lng, lat]
     });
 
     let restaurantsArr = [
@@ -42,7 +55,10 @@ $(document).ready(function(){
     restaurantsArr.forEach(function(restaurant){
         geocode(restaurant.address, MAPBOX_KEY_EX_1).then(function(result) {
             console.log(result);
-            map.setZoom(12);
+            restaurant.latLong = result;
+            restaurant.zoom = 16;
+            restaurant.pitch = 0;
+            restaurant.bearing = 0;
             let marker = new mapboxgl.Marker()
                 .setLngLat(result)
                 .addTo(map);
@@ -52,4 +68,77 @@ $(document).ready(function(){
             marker.setPopup(restaurantPopup);
         });
     });
+
+    $("#light-map").on("click", function(){
+        map.setStyle(lightMapStyle);
+    });
+
+    $("#dark-map").on("click", function(){
+        map.setStyle(darkMapStyle);
+    });
+
+    $("#home-location").click(updateMapLocation);
+
+    $("#location-1").click(updateMapLocation);
+    $("#location-2").click(updateMapLocation);
+    $("#location-3").click(updateMapLocation);
+
+
+    function updateMapLocation(){
+        console.log(this.value);
+        if(this.value == 0){
+            mapCenterLocation = homeLatLong
+            map.setCenter(homeLatLong);
+            map.setPitch(homePitch);
+            map.setBearing(homeBearing);
+            map.setZoom(homeZoom);
+        } else {
+            console.log("hello from else");
+            mapCenterLocation = restaurantsArr[this.value - 1].latLong;
+            map.setCenter(restaurantsArr[this.value - 1].latLong);
+            map.setPitch(restaurantsArr[this.value - 1].pitch);
+            map.setBearing(restaurantsArr[this.value - 1].bearing);
+            map.setZoom(restaurantsArr[this.value - 1].zoom);
+        }
+    }
+
+    $("#zoom-in-map").click(zoomIn);
+    $("#zoom-out-map").click(zoomOut);
+
+
+    function zoomIn(){
+        if(currentZoomIndex === zoomArr.length - 1){
+            currentZoomIndex = 0
+        } else {
+            currentZoomIndex++;
+        }
+        map.setZoom(zoomArr[currentZoomIndex]);
+        map.setCenter(mapCenterLocation);
+    }
+    function zoomOut(){
+        if(currentZoomIndex === 0){
+            currentZoomIndex = zoomArr.length - 1;
+        } else {
+            currentZoomIndex--;
+        }
+        map.setZoom(zoomArr[currentZoomIndex]);
+        map.setCenter(mapCenterLocation);
+    }
+
+    $("#address-search-button").click(searchAddress);
+
+    function searchAddress(){
+        geocode($("#address-search").val(), MAPBOX_KEY_EX_1).then(function(result) {
+            console.log(result);
+            mapCenterLocation = result;
+            map.setCenter(mapCenterLocation);
+            map.setZoom(12);
+            map.setPitch(0);
+            map.setBearing(0);
+            let marker = new mapboxgl.Marker()
+                .setLngLat(result)
+                .addTo(map);
+        });
+    }
+
 });

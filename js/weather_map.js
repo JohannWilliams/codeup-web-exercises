eef4422c0ca5aa207c6c250b81526ed4b59ad568
Johann Williams <johann.williams01@gmail.com>
2023-05-31 13:38:54 -0500

    displaying a map with austin at the center

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,22 @@
 "use strict";
 
-$(document).ready(function(){
-   //TODO : code out the weather map.
+$(document).ready(function () {
+    //TODO : code out the weather map.
+
+    /**
+     * create the map with a preset location and orientation.
+     */
+    mapboxgl.accessToken = MAPBOX_KEY;
+    let map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        zoom: 10, // starting zoom
+        center: [0,0] // [lng, lat]
+    });
+    geocode("Austin, Tx", MAPBOX_KEY).then(function (result) {
+        map.setCenter(result);
+        console.log(result);
+    });
+
 
 });

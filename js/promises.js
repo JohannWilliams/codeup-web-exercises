7d311114a6392ec7597814b3c14acd24999b8e36
Johann Williams <johann.williams01@gmail.com>
2023-06-07 14:53:45 -0500

    completed bonus for making my own promise

--- js/promises.js
+++ js/promises.js
@@ -9,13 +9,17 @@
     function wait(num){
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve();
+                resolve(num);
             }, num);
         });
     }
 
-    console.log(wait(1000).then(() => console.log('You\'ll see this after 1 second')));
-    console.log(wait(3000).then(() => console.log('You\'ll see this after 3 seconds')));
+    console.log(wait(1000).then((message) => console.log(`You'll see this after ${message/1000} second.`)));
+    console.log(wait(3000).then((message) => console.log(`You'll see this after ${message/1000} second.`)));
+    for(let i = 0; i < 10; i++){
+        console.log(wait(Math.floor(Math.random() * 5000) + 1000).then((message) => console.log(`You'll see this after ${message/1000} second. Loop index ${i}`)));
+    }
+
 
 
 })();

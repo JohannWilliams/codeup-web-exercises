f7f0d40edcf3d466eef684d99dbf391a3f9e336c
Johann Williams <johann.williams01@gmail.com>
2023-06-07 14:47:43 -0500

    completed my own promise

--- js/promises.js
+++ js/promises.js
@@ -1,5 +1,21 @@
 "use strict";
 
 (()=>{
+    /**
+     * Create Your Own Promise
+     * Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
+     */
+
+    function wait(num){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve();
+            }, num);
+        });
+    }
+
+    console.log(wait(1000).then(() => console.log('You\'ll see this after 1 second')));
+    console.log(wait(3000).then(() => console.log('You\'ll see this after 3 seconds')));
+
 
 })();
